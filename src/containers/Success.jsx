812db76fed3fd35ecee28c4a92cc25885d0b88d7
dloyolar <dieegolr@gmail.com>
2022-02-04 13:11:21 -0300

    [WIP] Success landing structure

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import '../styles/components/Success.css';
+
 export const Success = () => {
-  return <h1>Success</h1>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Diego, Thank you for your purchase</h2>
+        <span>Your order will arrive in 3 days to your address.</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
+    </div>
+  );
 };

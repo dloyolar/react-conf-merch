6c860dbdde22d9d912222957e1c5a246dc721cbf
dloyolar <dieegolr@gmail.com>
2022-02-04 13:11:16 -0300

    [WIP] Payment landing structure

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import '../styles/components/Payment.css';
+
 export const Payment = () => {
-  return <h1>Payment</h1>;
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Order summary</h3>
+        <div className="Payment-button">Pay with Paypal</div>
+      </div>
+      <div />
+    </div>
+  );
 };

cbb19132820ab7096ec0692cfc96dc8022bc902f
dloyolar <dieegolr@gmail.com>
2022-02-04 11:48:24 -0300

    [ADD] Layout, Header, Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+export const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">Conf Merch</p>
+      <p className="Footer-copy">All rights reserved</p>
+    </div>
+  );
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/components/Header.css'
+
+export const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="Header-title">Conf Merch</h1>
+      <div className="Header-checkout">Checkout</div>
+    </div>
+  );
+};

8d4c901e7bc26ce9fd4af6431f97c1e187d26e7f
dloyolar <dieegolr@gmail.com>
2022-02-04 12:55:07 -0300

    [WIP] Information landing

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import '../styles/components/Information.css';
 
 export const Information = () => {
-  return <h1>Information</h1>;
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Contact information:</h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="Full name" name="name" />
+            <input type="text" placeholder="Email" name="email" />
+            <input type="text" placeholder="Address" name="address" />
+            <input type="text" placeholder="Dept" name="apto" />
+            <input type="text" placeholder="City" name="city" />
+            <input type="text" placeholder="Country" name="country" />
+            <input type="text" placeholder="State" name="state" />
+            <input type="text" placeholder="Postal Code" name="zip" />
+            <input type="text" placeholder="Phone" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Back</div>
+          <div className="Information-next">Pay</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Order:</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>ITEM name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };

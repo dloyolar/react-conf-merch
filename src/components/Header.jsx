787cf078f44c2892d7197b3c5907be2bcf387163
dloyolar <dieegolr@gmail.com>
2022-02-04 13:26:57 -0300

    [REFACTOR] add Links and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import '../styles/components/Header.css'
+import { Link } from 'react-router-dom';
+import '../styles/components/Header.css';
 
 export const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header-title">Conf Merch</h1>
-      <div className="Header-checkout">Checkout</div>
+      <h1 className="Header-title">
+        <Link to={'/'}>Conf Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to={'/checkout'}>
+          <i className="fas fa-shopping-basket" />
+        </Link>
+      </div>
     </div>
   );
 };
--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Information.css';
 
 export const Information = () => {
@@ -23,7 +24,9 @@ export const Information = () => {
         </div>
         <div className="Information-buttons">
           <div className="Information-back">Back</div>
-          <div className="Information-next">Pay</div>
+          <Link to={'/checkout/payment'}>
+            <div className="Information-next">Pay</div>
+          </Link>
         </div>
       </div>
       <div className="Information-sidebar">

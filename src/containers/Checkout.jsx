c5b15188dda4c57bd197e1b099b3fb5a05e24e49
dloyolar <dieegolr@gmail.com>
2022-02-04 12:48:14 -0300

    [WIP] Checkout component

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
+import '../styles/components/Checkout.css';
+
 export const Checkout = () => {
-  return <h1>Checkout</h1>;
+  return (
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Order list:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Delete</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Total price: $10</h3>
+        <button type="button">Continue order</button>
+      </div>
+    </div>
+  );
 };

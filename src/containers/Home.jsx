a6aa669eee051e00211816066e454d7900a939ec
dloyolar <dieegolr@gmail.com>
2022-02-04 12:19:14 -0300

    [REFACTOR] use Products component in home with initial data

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Products } from '../components/Products';
+import initialState from '../initialState';
 
 export const Home = () => {
-  return <h1>Home</h1>;
+  return <Products products={initialState.products} />;
 };

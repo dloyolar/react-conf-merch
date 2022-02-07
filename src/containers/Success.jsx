46af4de39bbf52c7b22fb469b31440dbc8aaa9da
dloyolar <dieegolr@gmail.com>
2022-02-06 21:18:48 -0300

    [ADD] Add conditional map in Success

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -2,19 +2,26 @@ import React, { useContext } from 'react';
 import { Map } from '../components/Map';
 
 import { AppContext } from '../context/AppContext';
+import { useAddress } from '../hooks/useAddress.js';
 
 import '../styles/components/Success.css';
 
 export const Success = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const map = useAddress(buyer[0].address);
+
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{`${buyer.name}, Thank you for your purchase`}</h2>
+        <h2>{`${buyer[0]?.name}, Thank you for your purchase`}</h2>
         <span>Your order will arrive in 3 days to your address.</span>
         <div className="Success-map">
-          <Map />
+          {map && Object.keys(map).length !== 0 ? (
+            <Map data={map} />
+          ) : (
+            'Wait a second please...'
+          )}
         </div>
       </div>
     </div>

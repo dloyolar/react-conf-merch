cacadff5af9929a2f1bb8a493d153e25bff5c543
dloyolar <dieegolr@gmail.com>
2022-02-06 21:36:10 -0300

    [ADD] Seo component to Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+
 import { Products } from '../components/Products';
+import { Seo } from '../components/Seo';
 import initialState from '../initialState';
 
 export const Home = () => {
-  return <Products products={initialState.products} />;
+  return (
+    <>
+      <Seo title={'Conf Merch - Products'} />
+      <Products products={initialState.products} />
+    </>
+  );
 };
